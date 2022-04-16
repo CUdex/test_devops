e0edc48553c8e6254e1eb0a47eb7faa068467615
cudex <ryuchan-ul@lyuchan-ul-ui-MacBookPro.local>
2022-04-16 14:59:58 +0900

    create instance

--- main.tf
+++ main.tf
@@ -26,8 +26,6 @@ resource "aws_security_group" "test_security_group" {
 
 resource "aws_instance" "test" {
 
-  count         = 1
-
   ami           = "ami-0454bb2fefc7de534"
   instance_type = "t3.micro"
 #public ip와 연결
@@ -39,6 +37,27 @@ resource "aws_instance" "test" {
 #key pair 할당
   key_name = data.aws_key_pair.my_key.key_name
 
+  vpc_id = aws_vpc.main.id
+
+  subnet_id = aws_subnet.main.id
+
 #instance 이름
   tags = var.tags_map
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "172.30.0.0/16"
+
+  tags = {
+    Name = "RYU_VPC"
+  }
+}
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "172.30.1.0/24"
+
+  tags = {
+    Name = "RYU_SUBNET"
+  }
 }
