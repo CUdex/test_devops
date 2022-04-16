e953db783b71a8f543ff3f48696e7e1672c0386b
cudex <ryuchan-ul@lyuchan-ul-ui-MacBookPro.local>
2022-04-16 10:30:43 +0900

    test3

--- variable.tf
+++ variable.tf
@@ -1,13 +1,13 @@
-variable "image_id" {
-  type = string
-}
+# variable "image_id" {
+#   type = string
+# }
 
-variable "availability_zone_names" {
-  type    = list(string)
-  default = ["us-west-1a"]
-}
+# variable "availability_zone_names" {
+#   type    = list(string)
+#   default = ["us-west-1a"]
+# }
 
-variable "ami_id_maps" {
-  type = map
-  default = {}
-}
+# variable "ami_id_maps" {
+#   type = map
+#   default = {}
+# }
